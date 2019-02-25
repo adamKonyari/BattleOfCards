ca8bb91d0ae3af479cabae28a88fc170e8faa87a
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-02-25 10:21:47 +0100

    card,deck classes created, heroes xml file created

---
+++ src/main/java/com/codecool/Card.java
@@ -0,0 +1,63 @@
+package com.codecool;
+
+/*
+strength, endurance, intelligence, vitality
+
+
+*/
+
+public class Card implements Comparable<Card>{
+
+    private String name;
+    private String game;
+
+    private int strength;
+    private int endurance;
+    private int intellingence;
+    private int vitality;
+
+    public Card(String name, String game, int strength, int endurance, int intellingence, int vitality) {
+        this.name = name;
+        this.game = game;
+        this.strength = strength;
+        this.endurance = endurance;
+        this.intellingence = intellingence;
+        this.vitality = vitality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public int getIntellingence() {
+        return intellingence;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+
+    @Override
+    public int compareTo(Card o) {
+
+        return this.getName().compareTo(o.getName());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/com/codecool/Deck.java
@@ -0,0 +1,22 @@
+package com.codecool;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    private List<Card> cards;
+
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void shuffle(List<Card> cards) {
+
+        Collections.shuffle(cards);
+    }
+
+    public void sort(List<Card> cards) {
+        Collections.sort(cards);
+    }
+}
