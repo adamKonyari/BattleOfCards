cdf5846d57b39da91da954c42794d309cd736bb1
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-02-25 10:44:36 +0100

    xmlparser getDoc method

---
+++ src/main/java/com/codecool/XMLParser.java
@@ -0,0 +1,22 @@
+package com.codecool;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class XMLParser {
+
+    public Document getDoc(String xmlPath) throws ParserConfigurationException, IOException, SAXException {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(xmlPath);
+        doc.getDocumentElement().normalize();
+
+        return doc;
+    }
+}
