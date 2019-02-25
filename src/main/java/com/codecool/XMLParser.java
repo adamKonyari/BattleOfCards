d34d6b79509aef9a7e89e3729b912418cf1532a4
farkaspeti <farkas.peti97@gmail.com>
2019-02-25 11:34:26 +0100

    Xml Parser in progress

--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -1,12 +1,17 @@
 package com.codecool;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLParser {
 
@@ -18,5 +23,16 @@ public class XMLParser {
         doc.getDocumentElement().normalize();
 
         return doc;
+    }
+
+    public List<Card> CardParser(Document doc) {
+        List<Card> cardList = new ArrayList<>();
+        NodeList nodeList = doc.getElementsByTagName("Hero");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node tempNode = nodeList.item(i);
+            Element tempElement = (Element) tempNode;
+
+        }
+        return cardList;
     }
 }
